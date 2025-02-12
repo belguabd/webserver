efc579016aae68e53c17cfaa4eb51d8179b13e0d
hassanmagueri <hassanmagueri2001@gmail.com>
2025-02-12 12:06:39 +0100

    add getClients

--- main.cpp
+++ main.cpp
@@ -7,18 +7,11 @@ int main()
     while(true)
     {
         server.run();
-        // for (std::map<int, httpRequest*>::const_iterator it = server.getConnectedClients().begin();it != server.getConnectedClients().end(); ++it)
-        // {
-        //     int client_id = it->first;
-        //     // httpRequest *clientRequest = it->second;
-        //     httpRequest clientRequest1 ;
-        //     httpRequest *clientRequest = it->second;
-        // //    clientRequest1.setclient(*clientRequest);
-
-            // std::cout<<"--->"<<client_id <<std::endl;
-
-        // }
-        // std::cout <<"  ----> "<<httpRequest.client.getfd();
+        std::map<int, httpRequest *> &map = server.getClients();
+        for (std::map<int, httpRequest *>::iterator it = map.begin(); it != map.end(); ++it)
+        {
+            std::cout << "->>>key: " << it->first << std::endl;
+        }
     }
     
 }
